866521fe90b0e9f59def26503724401281b18ee1
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 09:33:26 +0300

    Update copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { isExist, filesFolderPath, __dirname } from './helpers.js';
+
 const copy = async () => {
-    // Write your code here 
+    const destDirName = 'files_copy';
+    const destDirPath = path.join(__dirname, destDirName);
+
+    const isSourceExist = await isExist(filesFolderPath);
+    const isDestExist = await isExist(destDirPath);
+
+    if (!isSourceExist || isDestExist) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await fs.cp(filesFolderPath, destDirPath, { recursive: true });
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await copy();
