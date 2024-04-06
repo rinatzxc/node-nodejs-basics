d584d0046e1bcf8acb07915b804777729d9e9e5e
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 09:34:06 +0300

    Update rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { isExist, filesFolderPath } from './helpers.js';
+
 const rename = async () => {
-    // Write your code here 
+    const wrongFileName = 'wrongFilename.txt';
+    const properFileName = 'properFilename.md';
+
+    const wrongFilePath = path.join(filesFolderPath, wrongFileName);
+    const properFilePath = path.join(filesFolderPath, properFileName);
+
+    const isWrongFileExists = await isExist(wrongFilePath);
+    const isProperFileExists = await isExist(properFilePath);
+
+    if (!isWrongFileExists || isProperFileExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        fs.rename(wrongFilePath, properFilePath);
+    }
+    catch (err) {
+        console.error(err);
+    }
 };
+
 
 await rename();
