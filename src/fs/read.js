b6ddb55e61e11b09c5780a2cc4ef63f63bbfcf83
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 09:35:47 +0300

    Update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { isExist, filesFolderPath } from './helpers.js';
+
 const read = async () => {
-    // Write your code here 
+    const fileName = 'fileToRead.txt';
+
+    const filePath = path.join(filesFolderPath, fileName);
+    const isFileExists = await isExist(filePath);
+
+    if (!isFileExists) {
+        throw new Error('FS operation failed');
+    }
+
+    const content = await fs.readFile(filePath, 'utf-8');
+    console.log(content);
 };
 
 await read();
