a1de4ad3e12785c4538f0b5c5f60feb2ac507042
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 11:31:59 +0300

    Update cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import { fork } from 'child_process';
+
+import { filesFolderPath } from "./helpers.js";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const fileName = 'script.js';
+    const filePath = path.join(filesFolderPath, fileName);
+
+    const cp = fork(filePath, args, {
+        stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+    });
+
+    process.stdin.on('data', (data) => {
+        cp.stdin.write(data);
+    })
+
+    cp.stdout.on('data', (data) => {
+        process.stdout.write(data);
+    })
 };
 
 // Put your arguments in function call to test this functionality
