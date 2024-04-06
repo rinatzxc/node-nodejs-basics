bff29636c61dc4049d7c992399a3d542c823d1bb
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 11:18:10 +0300

    Update transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { pipeline } from 'stream/promises';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    await pipeline(
+        process.stdin,
+        new Transform({
+            transform(chunk, encoding, callback) {
+                const string = chunk.toString().trim();
+                const reversedString = string.split('').reverse().join('');
+                callback(null, `${reversedString}\n`);
+            }
+        }),
+        process.stdout,
+    )
 };
 
 await transform();
