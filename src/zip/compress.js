01204ad6ce13f0ca3d1744b14a26be1b23300e31
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 11:23:53 +0300

    Update compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import fs from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
+import { filesFolderPath } from "./helpers.js";
+
 const compress = async () => {
-    // Write your code here 
+    const sourceFileName = "fileToCompress.txt";
+    const destFileName = "archive.gz";
+
+    const sourceFilePath = path.join(filesFolderPath, sourceFileName);
+    const destFilePath = path.join(filesFolderPath, destFileName);
+
+    const readStream = fs.createReadStream(sourceFilePath);
+    const writeStream = fs.createWriteStream(destFilePath);
+
+    await pipeline(
+        readStream,
+        createGzip(),
+        writeStream
+    );
 };
 
 await compress();
