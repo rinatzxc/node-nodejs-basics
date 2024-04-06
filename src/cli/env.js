2f612a116a4ef21735af5d5e62071528fa7b5867
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 10:23:16 +0300

    Update env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const resultArray = [];
+
+    for (let [key, value] of Object.entries(process.env)) {
+        if (!key.startsWith(prefix)) {
+            continue;
+        }
+
+        resultArray.push(`${key}=${value}`);
+    }
+
+    console.log(resultArray.join('; '));
 };
 
 parseEnv();
