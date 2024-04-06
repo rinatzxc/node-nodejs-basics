af51d737e224257e114779c64efff4663daa055e
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 11:17:41 +0300

    Update write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+
+import { filesFolderPath } from "./helpers.js";
+
 const write = async () => {
-    // Write your code here 
+    const fileName = "fileToWrite.txt";
+    const filePath = path.join(filesFolderPath, fileName);
+
+    const writeStream = fs.createWriteStream(filePath);
+
+    await pipeline(
+        process.stdin,
+        writeStream
+    );
 };
 
 await write();
