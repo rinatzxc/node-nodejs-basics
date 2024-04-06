2525b526b6c62ca33ff514bcaa215911b20138e4
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 09:34:40 +0300

    Update delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { isExist, filesFolderPath } from './helpers.js';
+
 const remove = async () => {
-    // Write your code here 
+    const fileName = 'fileToRemove.txt';
+
+    const filePath = path.join(filesFolderPath, fileName);
+    const isFileExists = await isExist(filePath);
+
+    if (!isFileExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        fs.unlink(filePath)
+    }
+    catch (err) {
+        console.error(err);
+    }
 };
 
 await remove();
