c93c8fedbb9418fbdd259cced7bd67451027fdd2
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 10:22:34 +0300

    Update args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const resultArray = [];
+
+    for (let i = 0; i < args.length; i++) {
+        const arg = args[i];
+
+        if (arg.startsWith('--')) {
+            resultArray.push(`${arg.slice(2)} is ${args[i + 1]}`);
+            i++;
+        }
+    }
+
+    console.log(resultArray.join(', '))
 };
 
 parseArgs();
