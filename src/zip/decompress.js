42181649a07fbd95084635a44c0e7389218972aa
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 11:24:17 +0300

    Update decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGunzip } from 'zlib';
+
+import { filesFolderPath } from "./helpers.js";
+
 const decompress = async () => {
-    // Write your code here 
+    const sourceFileName = "archive.gz";
+    const destFileName = "fileToCompress.txt";
+
+    const sourceFilePath = path.join(filesFolderPath, sourceFileName);
+    const destFilePath = path.join(filesFolderPath, destFileName);
+
+    const readStream = fs.createReadStream(sourceFilePath);
+    const writeStream = fs.createWriteStream(destFilePath);
+
+    await pipeline(
+        readStream,
+        createGunzip(),
+        writeStream
+    )
 };
 
 await decompress();
