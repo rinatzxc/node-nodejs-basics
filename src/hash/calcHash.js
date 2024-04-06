628ecc51381564bafa8283b4b022b29e9ef19ab3
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 10:42:47 +0300

    Update calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import { createHash } from 'crypto';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const folderName = 'files';
+const folderPath = path.join(__dirname, folderName);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileName = 'fileToCalculateHashFor.txt';
+    const filePath = path.join(folderPath, fileName);
+
+    const hash = createHash('sha256');
+    const input = createReadStream(filePath);
+
+    input.on('data', data => hash.update(data));
+    input.on('end', () => {
+        const digest = hash.digest('hex');
+        console.log(digest);
+    })
 };
 
 await calculateHash();
