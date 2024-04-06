a28ab0a125c79857f365c5e2d18245e52c73912e
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 09:29:45 +0300

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { isExist, filesFolderPath } from './helpers.js';
+
 const create = async () => {
-    // Write your code here 
+    const fileName = 'fresh.txt';
+    const content = 'I am fresh and young';
+
+    const filePath = path.join(filesFolderPath, fileName);
+
+    if (await isExist(filePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await fs.writeFile(filePath, content);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await create();
