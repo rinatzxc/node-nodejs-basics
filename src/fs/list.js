e8cf99735b349306c44b941fc6e19575c56e5687
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-04-06 09:35:15 +0300

    Update list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+
+import { isExist, filesFolderPath } from './helpers.js';
+
 const list = async () => {
-    // Write your code here 
+    const isFolderExists = await isExist(filesFolderPath);
+
+    if (!isFolderExists) {
+        throw new Error('FS operation failed');
+    }
+
+    const content = await fs.readdir(filesFolderPath);
+
+    for (let fileName of content) {
+        console.log(fileName);
+    }
 };
 
 await list();
